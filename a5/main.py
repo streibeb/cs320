57be5ec6b51fcede2f72cd120b5b9e32b24dae56
Brayden Streibel <brayden@streibel.ca>
2016-12-03 20:36:46 -0600

    fixed

--- a5/main.py
+++ a5/main.py
@@ -6,44 +6,42 @@ def main(argv):
     parser.add_argument("-t", "--test", type=int, choices=range(0,3))
     args = parser.parse_args()
 
-    test = 0
+    testOpt = 0
     if args.test != None:
-        test = args.test
+        testOpt = args.test
 
-    if test == 0 or test == 1:
+    if testOpt == 0 or testOpt == 1:
         print '\n##############################################'
         print '# TEST  1                                    #'
         print '##############################################'
-        test1()
+        data = [
+            [0,1,0,0],
+            [1,1,0,1],
+            [1,1,1,0],
+            [1,1,1,1]
+        ]
+        test(data)
 
-    if test == 0 or test == 2:
+    if testOpt == 0 or testOpt == 2:
         print '\n##############################################'
         print '# TEST  2                                    #'
         print '##############################################'
-        test2()
+        data = [
+            [0,1,0,0],
+            [0,1,0,1],
+            [0,1,1,0],
+            [1,1,1,1]
+        ]
+        test(data)
 
-def test1():
-    data = [
-        [0,1,0,0],
-        [1,1,0,1],
-        [1,1,1,0],
-        [1,1,1,1]
-    ]
-
+def test(data):
     p = Perceptron()
-    p.learn(data)
+    w = p.learn(data)
 
-def test2():
-    data = [
-        [0,1,0,0],
-        [0,1,0,1],
-        [0,1,1,0],
-        [1,1,1,1]
-    ]
-
-    p = Perceptron()
-    p.learn(data)
-
+    print '\nFinal Result'
+    for i in range(len(data)):
+        print 'x[{0}]: {1}'.format(i, data[i])
+    print '   w: {0}'.format(w)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
--- a5/perceptron.py
+++ a5/perceptron.py
@@ -9,26 +9,31 @@ class Perceptron:
         print ' i  | x          | w          |  x[0] | x * w '
         print '----+------------+------------+-------+-------'
 
-        i = 0
-        while not self.check(data):
-            # print '\n============= Iteration {0:2} ============='.format(i+1)
+        counter = 0
+        for i in range(1000):
+            if (counter == len(data)-1):
+                break
+
             x = np.array(data[i % len(data)])
             dot = np.dot(x[1:], self.w)
-            print '{0:3} | {1:10} | {2:10} | {3:5} | {4:5}'.format(i+1, x[1:], self.w, x[0], np.dot(x[1:], self.w))
+            # print 'Iteration {0}'.format(i+1)
+            # print 'x: {0:15} w: {1:15}'.format(x[1:], self.w)
             if (x[0] == 1):
-                if (np.dot(x[1:], self.w) < 0):
+                # print 'Positive x[0]'
+                if (dot < 0):
                     self.w = np.add(self.w, x[1:])
+                    # print 'Corrected w: {0}'.format(self.w)
+                    counter = 0
+                else:
+                    counter += 1
             else:
-                if (np.dot(x[1:], self.w) >= 0):
+                # print 'Negative x[0]'
+                if (dot >= 0):
                     self.w = np.subtract(self.w, x[1:])
-            i += 1
+                    # print 'Corrected w: {0}'.format(self.w)
+                    counter = 0
+                else:
+                    counter += 1
 
-    def check(self, data):
-        for x in data:
-            dot = np.dot(x[1:], self.w)
-
-            if x[0] == 1 and dot < 0:
-                return False
-            elif x[0] == 0 and dot >= 0:
-                return False
-        return True
+            print '{0:3} | {1:10} | {2:10} | {3:5} | {4:5}'.format(i+1, x[1:], self.w, x[0], dot)
+        return self.w
